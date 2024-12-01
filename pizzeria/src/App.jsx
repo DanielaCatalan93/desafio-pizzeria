9c0f25e4697e5f3943956cb8d14e38ae02b3e925
DanielaCatalan93 <dcdaniela93@gmail.com>
2024-11-30 21:35:01 -0300

    cart

--- pizzeria/src/App.jsx
+++ pizzeria/src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Register from "./pages/components/Register";
-import Login from "./pages/components/Login";
+// import Register from "./pages/components/Register";
+// import Login from "./pages/components/Login";
+import Cart from "./pages/components/Cart";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      {/* <Home></Home>
-      <Register></Register>  */}
-      <Login></Login>
+      <Cart></Cart>
+      {/*  <Home></Home>
+       <Register></Register>  
+      <Login></Login> */}
       <Footer></Footer>
     </>
   );
--- pizzeria/src/pages/components/CardPizza.jsx
+++ pizzeria/src/pages/components/CardPizza.jsx
@@ -1,20 +1,22 @@
 import { formateador } from "../../utils/formateador";
 
-function CardPizza(props) {
+function CardPizza({ pizza }) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white border-gray-300 border-2 m-2 p-2">
-      <img className="w-full" src={props.img} alt="pizza" />
+      <img className="w-full" src={pizza.img} alt="pizza" />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl">{props.name}</div>
+        <div className="font-bold text-xl">{pizza.name}</div>
         <hr />
         <p className="text-center">Ingredientes:</p>
-        <p className="text-gray-700 text-center text-sm">
-          🍕 {props.ingredients.join(", ")}
-        </p>
+        <ul className="text-gray-700 text-center text-sm">
+          {pizza.ingredients.map((ingredient, index) => (
+            <li key={index}>🍕 {ingredient}</li>
+          ))}
+        </ul>
       </div>
       <hr />
       <p className="text-center text-2xl font-semibold">
-        Precio ${formateador(props.price)}
+        Precio ${formateador(pizza.price)}
       </p>
       <div className=" flex justify-around px-6 pt-4 pb-2">
         <button className="bg-white text-black py-2 px-4 rounded border-black border-2">
